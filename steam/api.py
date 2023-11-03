61d61406e597584aefbb6f355c2d032869ad4bd5
michaeln273 <148905089+michaeln273@users.noreply.github.com>
2023-11-03 11:33:57 -0400

    Comment api.py

--- steam/api.py
+++ steam/api.py
@@ -7,6 +7,9 @@ urls = {
     "search": "/storesearch"
 }
 
+# Sends a get request to base steam api url plus the location passed to the function
+# Each request requires the wanted language and country code as url parameters. This function automatically sends
+# those parameters as well as the parameters passed to this function
 def steam_request(location, params={}):
     full_url = base_url + location
     response = requests.get(full_url, params={"l": "english", "cc": "us", **params})
